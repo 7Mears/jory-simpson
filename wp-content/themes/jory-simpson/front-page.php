fb536d85c966e16d30bf715312f397c70ef5e4c0
7Mears <7mears@gmail.com>
2014-10-21 10:27:53 -0600

    Created child theme

---
+++ wp-content/themes/jory-simpson/front-page.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * This file adds the Home Page to the Jory Simpson Theme.
+ *
+ * @author EndBoss
+ * @package Jsim
+ * @subpackage Customizations
+ */
+
+add_action( 'wp_enqueue_scripts', 'jsim_enqueue_home_scripts' );
+/**
+ * Enqueue Scripts
+ */
+function jsim_enqueue_home_scripts() {
+
+	wp_enqueue_script( 'jsim-home', get_bloginfo( 'stylesheet_directory' ) . '/js/home.js', array( 'jquery' ), '1.0.0', true );
+	wp_enqueue_script( 'localScroll', get_stylesheet_directory_uri() . '/js/jquery.localScroll.min.js', array( 'scrollTo' ), '1.2.8b', true );
+	wp_enqueue_script( 'scrollTo', get_stylesheet_directory_uri() . '/js/jquery.scrollTo.min.js', array( 'jquery' ), '1.4.5-beta', true );
+
+}
+
+add_action( 'genesis_meta', 'jsim_home_genesis_meta' );
+/**
+ * Add widget support for homepage. If no widgets active, display the default loop.
+ *
+ */
+function jsim_home_genesis_meta() {
+
+	if ( is_active_sidebar( 'hero-section' ) || is_active_sidebar( 'about-section' ) || is_active_sidebar( 'portfolio-section' ) ) {
+
+		//* Force full width content layout
+		add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+
+		//* Add jsim-pro-home body class
+		add_filter( 'body_class', 'jsim_body_class' );
+
+		//* Remove breadcrumbs
+		remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+
+		//* Remove the default Genesis loop
+		remove_action( 'genesis_loop', 'genesis_do_loop' );
+
+		//* Add home featured widget
+		add_action( 'genesis_after_header', 'jsim_home_featured_widget', 1 );
+
+		//* Add home widgets
+		add_action( 'genesis_before_footer', 'jsim_home_widgets', 5 );
+
+	}
+}
+
+function jsim_body_class( $classes ) {
+
+	$classes[] = 'jsim-pro-home';
+	return $classes;
+
+}
+
+function jsim_home_featured_widget() {
+
+	genesis_widget_area( 'hero-section', array(
+		'before' => '<div id="hero" class="hero-section"><div class="wrap">',
+		'after'  => '</div></div>',
+	) );
+
+}
+
+function jsim_home_widgets() {
+
+	echo '<div id="home-widgets" class="home-widgets">';
+	
+	genesis_widget_area( 'portfolio-section', array(
+		'before' => '<div id="portfolio" class="portfolio-section wrap">',
+		'after'  => '</div>',
+	) );
+
+	genesis_widget_area( 'about-section', array(
+		'before' => '<div id="about" class="about-section wrap">',
+		'after'  => '</div>',
+	) );
+
+	echo '</div>';
+
+}
+
+genesis();
---
+++ wp-content/themes/jory-simpson/functions.php
@@ -0,0 +1,58 @@
+<?php
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', 'Jory Simpson Theme' );
+define( 'CHILD_THEME_URL', 'http://www.endboss.ca' );
+define( 'CHILD_THEME_VERSION', '1.0' );
+
+//* Enqueue Google Fonts
+add_action( 'wp_enqueue_scripts', 'jsim_google_fonts' );
+function jsim_google_fonts() {
+
+	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Montserrat|Open+Sans:700,300', array(), 1.0 );
+}
+
+
+//* Enqueue and Load Font Awesome
+
+add_action( 'wp_enqueue_scripts', 'afn_enqueue_awesome' );
+function afn_enqueue_awesome() {
+	if ( !is_admin() ) {
+		wp_enqueue_style( 'afn-font-awesome', get_bloginfo( 'stylesheet_directory' ) . '/css/font-awesome.min.css', array(), '4.0.3' );
+	}
+}
+
+//* Add HTML5 markup structure
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
+
+//* Add new image sizes
+add_image_size( 'portfolio-section', 800, '800', TRUE );
+
+//* Add viewport meta tag for mobile browsers
+add_theme_support( 'genesis-responsive-viewport' );
+
+//* Add support for custom background
+add_theme_support( 'custom-background' );
+
+//* Add support for 3-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+
+//* Register widget areas
+genesis_register_sidebar( array(
+	'id'          => 'hero-section',
+	'name'        => __( 'Hero Section', 'jsim' ),
+	'description' => __( 'This is the first section of the home page.', 'jsim' ),
+) );
+genesis_register_sidebar( array(
+	'id'          => 'portfolio-section',
+	'name'        => __( 'Portfolio Section', 'jsim' ),
+	'description' => __( 'This is the fourth section of the home page.', 'jsim' ),
+) );
+genesis_register_sidebar( array(
+	'id'          => 'about-section',
+	'name'        => __( 'About Section', 'jsim' ),
+	'description' => __( 'This is the second section of the home page.', 'jsim' ),
+) );
