34e4e74c8df0fb52332d8e01ac89583eb3a2359b
7mears <7mears@gmail.com>
2014-11-01 19:19:56 -0600

    newest db - nov1

--- test_db.php
+++ test_db.php
@@ -1,2 +1,2 @@
- <?php  $connect=mysql_connect("localhost","root","root") or die("Unable to Connect");
+ <?php  $connect=mysql_connect("104.236.57.119","root","root") or die("Unable to Connect");
         mysql_select_db("jsim_db") or die("Could not open the db");
